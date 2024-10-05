4547322bd8bee46e21e4a50337f5b45ede105b02
lukamrko <lukamrko@gmail.com>
2024-10-05 15:08:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CSV_Obrada
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Insert path to CSV file:");
+            var filePath = Console.ReadLine();
+
+            while (!File.Exists(filePath))
+            {
+                Console.WriteLine($"CSV file doesn't exits at the given file path: {filePath}");
+                filePath = Console.ReadLine();
+            }
+
+
+
+
+
+        }
+    }
+}

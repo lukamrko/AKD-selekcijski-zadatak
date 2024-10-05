9e36e607ab8761742895edef9d4387ca713e487a
lukamrko <lukamrko@gmail.com>
2024-10-05 16:11:29 +0200

    First rought solution for the problem

--- Program.cs
+++ Program.cs
@@ -2,31 +2,45 @@
 {
     class Program
     {
-        private const char csvSeparator = ',';
+        private const char _csvSeparator = ',';
 
         static void Main()
         {
             var csvFilePath = GetCSVFilePath();
+            var usersData = GetUsersData(csvFilePath);
+            var fullHeader = "Ime, Prezime, DatumRodjenja, Ime(hex), Prezime(hex), DatumRodjenja(hex)";
 
-            using (var reader = new StreamReader(csvFilePath))
+            var folderPath = Path.GetDirectoryName(csvFilePath);
+            var newFileName = $"{Guid.NewGuid().ToString()}.csv";
+            var newFilePath = Path.Combine(folderPath, newFileName);
+
+            var modifiedLines = new List<string> { fullHeader };
+            foreach(var userData in usersData) 
             {
-                var headerLine = reader.ReadLine();
-                Console.WriteLine(headerLine);
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    var lineValues = line.Split(csvSeparator);
-
-                    var firstName = lineValues[0].ToUpper();
-                    var lastName = lineValues[1].ToUpper();
-                    var birthDate = DateTime.Parse(lineValues[2]);
-
-
-                }
+                modifiedLines.Add(userData.ToCsvLine());
             }
-                
 
+            WriteAndShowData(newFilePath, modifiedLines);
+        }
 
+        private static void WriteAndShowData(string newFilePath, List<string> modifiedLines)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(newFilePath))
+                {
+                    foreach (var line in modifiedLines)
+                    {
+                        Console.WriteLine(line);
+                        writer.WriteLine(line);
+                    }
+                }
+                Console.WriteLine($"File saved successfully at: {newFilePath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
         }
 
         private static string GetCSVFilePath()
@@ -41,6 +55,34 @@
             }
 
             return csvFilePath;
+        }
+
+        private static List<UserData> GetUsersData(string csvFilePath)
+        {
+            var usersData = new List<UserData>();
+            using (var reader = new StreamReader(csvFilePath))
+            {
+                var headerLine = reader.ReadLine();
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var lineValues = line.Split(_csvSeparator);
+
+                    var firstName = lineValues[0].ToUpper();
+                    var lastName = lineValues[1].ToUpper();
+                    var birthDate = DateTime.Parse(lineValues[2]);
+
+                    var userData = new UserData
+                    {
+                        Name = firstName,
+                        Surname = lastName,
+                        BirthDate = birthDate,
+                    };
+
+                    usersData.Add(userData);
+                }
+            }
+            return usersData;
         }
     }
 }
